a9bcb03ddb31610d0ad8c92356d3074444e7ed53
Carlos Chávez <carlos.chavez@evolvemediallc.com>
2021-03-30 19:39:23 -0700

    EvolveCorpSite Initial Files

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>EvolveMedia Landing Page Desktop</title>
+<link rel="stylesheet" type="text/css" id="applicationStylesheet" href="css/dist/EvolveMedia_Landing_Page_Desktop.css"/>
+<script type="text/javascript" src="js/app.js" id="applicationScript"></script>
+</head>
+<body>
+
+<div id="Evolve Media LCC">
+	
+	<?php include_once "header.html"; ?>
+
+	<?php include_once "CP.html"; ?>
+
+	<?php include_once "about-us.html"; ?>
+
+	<?php include_once "our-brands.php"; ?>
+
+	<?php include_once "our-history.html"; ?>
+
+	<?php include_once "our-leadership.html"; ?>
+
+	<?php include_once "contact-us.html"; ?>
+
+	<?php include_once "footer.html"; ?>
+	
+</div>
+</body>
+</html>
