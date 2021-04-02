ec6e2f51e0ad9b1118cd725598a742ef804958ef
Carlos Chávez <carlos.chavez@evolvemediallc.com>
2021-04-01 19:08:05 -0700

    Last day iteration Updates

--- index.php
+++ index.php
@@ -4,30 +4,59 @@
 <meta charset="utf-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>EvolveMedia Landing Page Desktop</title>
-<link rel="stylesheet" type="text/css" id="applicationStylesheet" href="css/backup-css.css"/>
+<title>Evolve Media, LLC.</title>
+<meta name="description" content="Evolve Media is a publisher of leading enthusiast destinations for men and women." />
+<link rel="canonical" href="https://www.evolvemediallc.com/" />
+
+<link rel="apple-touch-icon" sizes="57x57" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="144x144" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/android-icon-144x144.png">
+<link rel="icon" type="image/png" sizes="96x96" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/android-icon-96x96.png">
+<link rel="icon" type="image/png" sizes="72x72" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/android-icon-72x72.png">
+<link rel="icon" type="image/png" sizes="48x48" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/android-icon-48x48.png">
+<link rel="icon" type="image/png" sizes="36x36" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/android-icon-36x36.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/favicon-16x16.png">
+<link rel="manifest" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/manifest.json">
+<link rel="shortcut icon" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/favicon.ico">
+<link rel="apple-touch-icon" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/apple-icon.png">
+<link rel="apple-touch-icon-precomposed" href="https://www.evolvemediallc.com/wp-content/themes/evolve-media/apple-icon-precomposed.png">
+
+<link rel="stylesheet" type="text/css" href="css/dist/main.css"/>
 <script type="text/javascript" src="js/app.js" id="applicationScript"></script>
 </head>
 <body>
 
-<div id="Evolve Media LCC">
+<div class="wrapper">
 	
 	<?php include_once "header.html"; ?>
 
-	<?php include_once "CP.html"; ?>
+	<?php //include_once "CP.html"; ?>
 
 	<?php include_once "about-us.html"; ?>
 
 	<?php include_once "our-brands.php"; ?>
 
-	<?php include_once "our-history.html"; ?>
+	<?php //include_once "our-history.html"; ?>
 
 	<?php include_once "our-leadership.html"; ?>
 
-	<?php include_once "contact-us.html"; ?>
+	<?php //include_once "contact-us.html"; ?>
 
-	<?php include_once "footer.html"; ?>
+	<?php include_once "get-in-touch.html"; ?>
+
+	<?php include_once "footer.php"; ?>
 	
 </div>
+
 </body>
 </html>
