73f1f350ac7aa189658a63f919e35696b8b9b80b
berthamz <berthamz@gmail.com>
2021-04-05 12:03:05 -0500

    Loading svgs with img tag

--- our-brands.php
+++ our-brands.php
@@ -5,66 +5,66 @@
 	<div id="OurBrands-logos" class="our-brands-logos">
 
 		<div class="logo CT-logo">
-			<?php include_once "images/our-brands/cattime_logo_color.svg"; ?>
+			<img src="images/our-brands/cattime_logo_color.svg" alt="">
 		</div>
 			
 		<div class="logo CS-logo">
-			<?php include_once "images/our-brands/comingsoon_logo_color.svg"; ?>
+			<img src="images/our-brands/comingsoon_logo_color.svg" alt="">
 		</div>
 
 		<div class="logo DT-logo">
-			<?php include_once "images/our-brands/dogtime_logo_color.svg"; ?>
+			<img src="images/our-brands/dogtime_logo_color.svg" alt="">
 		</div>
 
 		<div class="logo GR-logo">
-			<?php include_once "images/our-brands/gamerevolution_logo_color.svg"; ?>
+			<img src="images/our-brands/gamerevolution_logo_color.svg" al="">
 		</div>
 			
 		<div class="logo LO-logo">
-			<?php include_once "images/our-brands/liveoutdoors_logo_color.svg"; ?>
+			<img src="images/our-brands/liveoutdoors_logo_color.svg" al="">
 		</div>
 
 		<div class="logo MD-logo">
-			<?php include_once "images/our-brands/mandatory_logo_color.svg"; ?>
+			<img src="images/our-brands/mandatory_logo_color.svg" al="">
 		</div>
 
 		<div class="logo MT-logo">
-			<?php include_once "images/our-brands/momtastic_logo_color.svg"; ?>
+			<img src="images/our-brands/momtastic_logo_color.svg" al="">
 		</div>
 		
 		<div class="logo RT-logo">
-			<?php include_once "images/our-brands/realiytea_logo_color.svg"; ?>
+			<img src="images/our-brands/realitytea_logo_color.svg" al="">
 		</div>
 
 		<div class="logo SD-logo">
-			<?php include_once "images/our-brands/sherdog_logo_color.svg"; ?>
+			<img src="images/our-brands/sherdog_logo_color.svg" al="">
 		</div>
 
 		<div class="logo SHH-logo">
-			<?php include_once "images/our-brands/superherohype_logo_color.svg"; ?>
+			<img src="images/our-brands/superherohype_logo_color.svg" al="">
 		</div>
 
 		<div class="logo TFS-logo">
-			<?php include_once "images/our-brands/thefashionspot_logo_color.svg"; ?>
+			<img src="images/our-brands/thefashionspot_logo_color.svg" al="">
 		</div>
 
 		<div class="logo hfboards-logo">
-			<?php include_once "images/our-brands/hfboards_logo_color.svg"; ?>
+			<img src="images/our-brands/hfboards_logo_color.svg" al="">
 		</div>
 
 		<div class="logo psls-logo">
-			<?php include_once "images/our-brands/psls_logo_color.svg"; ?>
+			<img src="images/our-brands/psls_logo_color.svg" al="">
 		</div>
 
 		<div class="logo musicfeeds-logo">
-			<?php include_once "images/our-brands/musicfeeds_logo_color.svg"; ?>
+			<img src="images/our-brands/musicfeeds_logo_color.svg" al="">
 		</div>
 
 		<div class="totalbeauty-logo">
-			<?php include_once "images/our-brands/totalbeauty_logo_color.svg"; ?>
+			<img src="images/our-brands/totalbeauty_logo_color.svg" al="">
 		</div>
 		<div class="wrestlezone-logo">
-			<?php include_once "images/our-brands/wrestlezone_logo_color.svg"; ?>
+			<img src="images/our-brands/wrestlezone_logo_color.svg" al="">
 		</div>
 	</div>
 	
