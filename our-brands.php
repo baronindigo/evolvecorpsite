7b1d04fecb9d4d55a83e3d30f2f6d2041b92887b
Carlos Chávez <carlos.chavez@evolvemediallc.com>
2021-04-05 10:43:02 -0700

    Anchors added to brands logos

--- our-brands.php
+++ our-brands.php
@@ -5,66 +5,98 @@
 	<div id="OurBrands-logos" class="our-brands-logos">
 
 		<div class="logo CT-logo">
-			<img src="images/our-brands/cattime_logo_color.svg" alt="">
+			<a href="https://cattime.com" target="_blank">
+				<img src="images/our-brands/cattime_logo_color.svg" alt="CatTime Logo">
+			</a>
 		</div>
 			
 		<div class="logo CS-logo">
-			<img src="images/our-brands/comingsoon_logo_color.svg" alt="">
+			<a href="https://comingsoon.net" target="_blank">
+				<img src="images/our-brands/comingsoon_logo_color.svg" alt="ComingSoon Logo">
+			</a>
 		</div>
 
 		<div class="logo DT-logo">
-			<img src="images/our-brands/dogtime_logo_color.svg" alt="">
+			<a href="https://dogtime.com" target="_blank">
+				<img src="images/our-brands/dogtime_logo_color.svg" alt="DogtTime Logo">
+			</a>
 		</div>
 
 		<div class="logo GR-logo">
-			<img src="images/our-brands/gamerevolution_logo_color.svg" al="">
+			<a href="https://gamerevolution.com" target="_blank">
+				<img src="images/our-brands/gamerevolution_logo_color.svg" alt="GameRevolution Logo">
+			</a>
 		</div>
 			
 		<div class="logo LO-logo">
-			<img src="images/our-brands/liveoutdoors_logo_color.svg" al="">
+			<a href="https://liveoutdoors.com" target="_blank">
+				<img src="images/our-brands/liveoutdoors_logo_color.svg" alt="Liveoutdoors Logo">
+			</a>
 		</div>
 
 		<div class="logo MD-logo">
-			<img src="images/our-brands/mandatory_logo_color.svg" al="">
+			<a href="https://mandatory.com" target="_blank">
+				<img src="images/our-brands/mandatory_logo_color.svg" alt="Mandatory Logo">
+			</a>
 		</div>
 
 		<div class="logo MT-logo">
-			<img src="images/our-brands/momtastic_logo_color.svg" al="">
+			<a href="https://momtastic.com" target="_blank">
+				<img src="images/our-brands/momtastic_logo_color.svg" alt="Momtastic Logo">
+			</a>
 		</div>
 		
 		<div class="logo RT-logo">
-			<img src="images/our-brands/realitytea_logo_color.svg" al="">
+			<a href="https://realitytea.com" target="_blank">
+				<img src="images/our-brands/realitytea_logo_color.svg" alt="RealityTea Logo">
+			</a>
 		</div>
 
 		<div class="logo SD-logo">
-			<img src="images/our-brands/sherdog_logo_color.svg" al="">
+			<a href="https://sherdog.com" target="_blank">
+				<img src="images/our-brands/sherdog_logo_color.svg" alt="Sherdog Logo">
+			</a>
 		</div>
 
 		<div class="logo SHH-logo">
-			<img src="images/our-brands/superherohype_logo_color.svg" al="">
+			<a href="https://superherohype.com" target="_blank">
+				<img src="images/our-brands/superherohype_logo_color.svg" alt="Superherohype Logo">
+			</a>
 		</div>
 
 		<div class="logo TFS-logo">
-			<img src="images/our-brands/thefashionspot_logo_color.svg" al="">
+			<a href="https://thefashionspot.com" target="_blank">
+				<img src="images/our-brands/thefashionspot_logo_color.svg" alt="TheFashionSport Logo">
+			</a>
 		</div>
 
 		<div class="logo hfboards-logo">
-			<img src="images/our-brands/hfboards_logo_color.svg" al="">
+			<a href="https://hfboards.mandatory.com/" target="_blank">
+				<img src="images/our-brands/hfboards_logo_color.svg" alt="Hockeys Future Boards Logo">
+			</a>
 		</div>
 
 		<div class="logo psls-logo">
-			<img src="images/our-brands/psls_logo_color.svg" al="">
+			<a href="https://playstationlifestyle.net" target="_blank">
+				<img src="images/our-brands/psls_logo_color.svg" alt=" PlayStationLifestyle Logo">
+			</a>
 		</div>
 
 		<div class="logo musicfeeds-logo">
-			<img src="images/our-brands/musicfeeds_logo_color.svg" al="">
+			<a href="https://musicfeeds.com.au" target="_blank">
+				<img src="images/our-brands/musicfeeds_logo_color.svg" alt="MusicFeeds Logo">
+			</a>
 		</div>
 
 		<div class="totalbeauty-logo">
-			<img src="images/our-brands/totalbeauty_logo_color.svg" al="">
+			<a href="https://totalbeauty.com" target="_blank">
+				<img src="images/our-brands/totalbeauty_logo_color.svg" alt="TotalBeauty Logo">
+			</a>
 		</div>
 		<div class="wrestlezone-logo">
-			<img src="images/our-brands/wrestlezone_logo_color.svg" al="">
+			<a href="https://mandatory.com/wrestlezone" target="_blank">
+				<img src="images/our-brands/wrestlezone_logo_color.svg" alt="Wrestlezone Logo">
+			</a>
 		</div>
 	</div>
 	
