0b6928badef499b1d6c41db5c609baa9df8d1dba
Carlos Chávez <carlos.chavez@evolvemediallc.com>
2021-04-07 14:38:00 -0700

    Images relocation

--- index.php
+++ index.php
@@ -7,28 +7,28 @@
 <title>Evolve Media, LLC.</title>
 <meta name="description" content="Evolve Media is a publisher of leading enthusiast destinations for men and women." />
 <link rel="canonical" href="https://www.evolvemediallc.com/" />
-<link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192" href="android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="144x144" href="android-icon-144x144.png">
-<link rel="icon" type="image/png" sizes="96x96" href="android-icon-96x96.png">
-<link rel="icon" type="image/png" sizes="72x72" href="android-icon-72x72.png">
-<link rel="icon" type="image/png" sizes="48x48" href="android-icon-48x48.png">
-<link rel="icon" type="image/png" sizes="36x36" href="android-icon-36x36.png">
-<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+<link rel="apple-touch-icon" sizes="57x57" href="/images/setup/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/images/setup/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/images/setup/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/images/setup/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/images/setup/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/images/setup/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/images/setup/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/images/setup/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/images/setup/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192" href="/images/setup/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="144x144" href="/images/setup/android-icon-144x144.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/images/setup/android-icon-96x96.png">
+<link rel="icon" type="image/png" sizes="72x72" href="/images/setup/android-icon-72x72.png">
+<link rel="icon" type="image/png" sizes="48x48" href="/images/setup/android-icon-48x48.png">
+<link rel="icon" type="image/png" sizes="36x36" href="/images/setup/android-icon-36x36.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/setup/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/images/setup/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/setup/favicon-16x16.png">
 <link rel="manifest" href="manifest.json">
-<link rel="shortcut icon" href="favicon.ico">
-<link rel="apple-touch-icon" href="apple-icon.png">
-<link rel="apple-touch-icon-precomposed" href="apple-icon-precomposed.png">
+<link rel="shortcut icon" href="/images/setup/favicon.ico">
+<link rel="apple-touch-icon" href="/images/setup/apple-icon.png">
+<link rel="apple-touch-icon-precomposed" href="/images/setup/apple-icon-precomposed.png">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@400;500&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/dist/main.css"/>
