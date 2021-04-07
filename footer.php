a1ca7fe70a466a4efb01599182950298f01de20f
berthamz <berthamz@gmail.com>
2021-04-07 18:42:56 -0500

    footer

--- footer.php
+++ footer.php
@@ -1,95 +1,13 @@
 <footer>
-	<img src="images/evolve-logo-white.svg" alt="Evolve Media LLC logo">
-
-	<a href="#">PRIVACY POLICY</a>
-	<a href="#">TERMS OF SERVICE</a>
-
-	<div class="linkedin-logo">
-		<img src="images/footer/linkedin-icon.svg" alt="Linkedin link">
+	<div class="footer-container">
+		<img class="footer-logo" src="images/evolve-logo-white.svg" alt="Evolve Media LLC logo">
+		<div class="footer-links">
+			<a href="#">PRIVACY POLICY</a>
+			<a href="#">TERMS OF SERVICE</a>
+			<a href="#">
+				<img  class="linkedin-logo" src="images/footer/linkedin-icon.svg" alt="Linkedin link">
+			</a>
+		</div>
+		<div class="footer-copyright">Evolve Media llc Company. &copy; <?php echo date( 'Y' ); ?> All Rights Reserved</div>
 	</div>
-
-	<div class="copyright">Evolve Media llc Company. &copy; <?php echo date( 'Y' ); ?> All Rights Reserved</div>
-
-	
-	<!--div id="Group_83">
-		<svg class="Rectangle_42">
-			<rect id="Rectangle_42" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_bm">
-			<rect id="Rectangle_42_bm" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_bn">
-			<rect id="Rectangle_42_bn" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_bo">
-			<rect id="Rectangle_42_bo" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_bp">
-			<rect id="Rectangle_42_bp" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_bq">
-			<rect id="Rectangle_42_bq" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_br">
-			<rect id="Rectangle_42_br" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_bs">
-			<rect id="Rectangle_42_bs" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_bt">
-			<rect id="Rectangle_42_bt" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_bu">
-			<rect id="Rectangle_42_bu" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_bv">
-			<rect id="Rectangle_42_bv" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_bw">
-			<rect id="Rectangle_42_bw" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-	</div-->
-	<!--div id="Group_84">
-		<svg class="Rectangle_42_by">
-			<rect id="Rectangle_42_by" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_bz">
-			<rect id="Rectangle_42_bz" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_b">
-			<rect id="Rectangle_42_b" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_ca">
-			<rect id="Rectangle_42_ca" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_cb">
-			<rect id="Rectangle_42_cb" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_cc">
-			<rect id="Rectangle_42_cc" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-		<svg class="Rectangle_42_cd">
-			<rect id="Rectangle_42_cd" rx="0" ry="0" x="0" y="0" width="65" height="10">
-			</rect>
-		</svg>
-	</div-->
-
 </footer>
